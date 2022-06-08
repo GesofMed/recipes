274dcd451f41528653920ac7b94e42419947089e
alexacron <alexjorj@gmail.com>
2022-06-08 18:23:59 +0300

    added files

---
+++ gesof/doc-bundle/2.0/assets/gesofdoc.js
@@ -0,0 +1,13 @@
+// gesofdoc.js
+const $ = require("jquery");
+require('webpack-jquery-ui');
+require('webpack-jquery-ui/css');
+
+require('bootstrap');
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './styles/gesofdoc.css';
+
+global.$ = global.jQuery = $;
+
+
